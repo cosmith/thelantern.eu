e615dbe0aec99cd1084c2f70997de5b13ce1f212
Corentin Smith <corentin@truckfly.com>
2019-04-25 22:04:13 +0200

    Enable netlify admin

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,13 +4,14 @@ import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import ArticlePreview from "./preview-templates/ArticlePreview";
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
 
-init({
-    config: {
-        backend: {
-            name: "test-repo",
-        },
-    },
-});
+// To test locally
+// init({
+//     config: {
+//         backend: {
+//             name: "test-repo",
+//         },
+//     },
+// });
 
 CMS.registerPreviewTemplate("index", IndexPagePreview);
 CMS.registerPreviewTemplate("about", AboutPagePreview);
